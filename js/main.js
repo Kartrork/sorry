a7af0c132c61f5aa619906b92b3bdeed84935442
unknown <Kartrork.Thy@pnc.lan>
2024-12-18 09:48:41 +0700

    #

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const h3 = document.getElementById('h3');
+btn = document.getElementById('btn');
+function Text(){
+    h3.textContent = 'ចុះហេតុអីមិនស្តាប់ចឹង បានអីមកអំណះអំណាងបញ្ជាក់'
+}
+btn.addEventListener('click',Text)
